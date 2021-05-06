c6b40b7107af4b31c13fdaf9867c2576a1316c4d
Ravi Ranjan Kumar <raviranjanhundred@gmail.com>
2021-05-06 21:13:35 +0530

    Add files via upload

---
+++ currency-conversion-service/src/main/java/com/spring/demo/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,57 @@
+package com.spring.demo.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+
+	private Logger logger=LogManager.getLogger(this.getClass());
+
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversionBean> responseEntity=new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+				CurrencyConversionBean.class, uriVariables);
+		CurrencyConversionBean response=responseEntity.getBody();
+
+		response.setQuantity(quantity);
+		response.setTotalCalculatedAmount(response.getConversionMultiple().multiply(quantity));
+		return response;
+
+	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		CurrencyConversionBean response=proxy.retrieveExchangeValue(from, to);
+
+		response.setQuantity(quantity);
+		response.setTotalCalculatedAmount(response.getConversionMultiple().multiply(quantity));
+		
+		logger.info("{}",response);
+		
+		return response;
+
+	}
+
+}
---
+++ currency-conversion-service/src/main/java/com/spring/demo/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,15 @@
+package com.spring.demo.currencyconversionservice;
+
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "springcloud-gateway-server")
+@LoadBalancerClient(name = "springcloud-gateway-server")
+public interface CurrencyExchangeServiceProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
